78420e9371e0f0e0d0ed2dd490a7dc987467125e
Creakfreak <jayatirai3@gmail.com>
2026-06-18 21:44:40 +0530

    saving current changes

---
+++ src/main/java/com/example/Day_8/MaximumSumOfDistinctSubarrayOfSizeK.java
@@ -0,0 +1,11 @@
+package com.example.Day_8;
+
+public class MaximumSumOfDistinctSubarrayOfSizeK {
+
+    int maximumSumSubarrayOfSizeK(int nums[])
+    {
+        int max=0;
+        return max;
+    }
+    
+}
